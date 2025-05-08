0a87fb1b00ad6ff64bc6f646dac1ee6cf7211233
RoZ1n7 <162337783+RoZ1n7@users.noreply.github.com>
2025-05-08 09:37:24 +0300

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -28,3 +28,11 @@ def test_divide(calc):
 def test_divide_by_zero(calc):
     with pytest.raises(ValueError, match="Cannot divide by zero."):
         calc.divide(10, 0)
+        
+def test_invalid_input_add(calc):
+    with pytest.raises(TypeError):
+        calc.add("a", 5)
+
+def test_invalid_input_divide(calc):
+    with pytest.raises(TypeError):
+        calc.divide(10, "b")
